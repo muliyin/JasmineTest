5c86740fd64c85514fea5b0383d106890ed259b1
棠梨煎雪 <247396282@qq.com>
2019-01-16 10:49:51 +0800

    package.json

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,8 +1,3 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
 
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
